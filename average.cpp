fab25e84118d0e285abe2694b7d344d3b02a4388
ggxgr.nonny47@gmail.com <Gaymaster@non.com>
2024-02-27 12:25:14 +0700

    Completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
 
+int main(int argc, char *argv[])
+{
+
+    double sum;
+    if (argc == 1)
+    {
+        cout << "Please input numbers to find average.";
+    }
+    else
+    {
+
+        for (int i = 0; i < argc - 1; i++)
+        {
+            sum += atof(argv[i + 1]);
+        }
+        cout << "---------------------------------" << endl;
+        cout << "Average of " << argc - 1 << " numbers"
+             << " "
+             << "= " << sum / (argc - 1) << "\n";
+        cout << "---------------------------------";
+    }
+}
